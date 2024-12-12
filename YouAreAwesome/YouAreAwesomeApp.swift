97ee38e7bc49a9d952b01e300d858dd2351df103
Hinalkharva <hinalkharva5@gmail.com>
2024-12-12 13:12:47 +0530

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Hinal Rajubhai Kharva on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YouAreAwesomeTests/YouAreAwesomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeTests.swift
+//  YouAreAwesomeTests
+//
+//  Created by Hinal Rajubhai Kharva on 12/12/24.
+//
+
+import Testing
+@testable import YouAreAwesome
+
+struct YouAreAwesomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
